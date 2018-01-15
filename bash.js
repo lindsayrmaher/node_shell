623be3731e323b868c83ad5f051603307128e1e6
Lindsay Maher <lindsaymah3r@gmail.com>
2018-01-15 10:32:51 -0500

    looked at process variable

---
+++ bash.js
@@ -0,0 +1,7 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', function (data) {
+	var cmd = data.toString().trim();
+	process.stdout.write('You typed: ' + cmd);
+	process.stdout.write('/nprompt > ');
+});
